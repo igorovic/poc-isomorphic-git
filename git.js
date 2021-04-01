b4e1ed6386d1648f3abbe62dcd1e2c5f5e287179
Mr. Test <mrtest@example.com>
2021-04-01 18:38:36 +0000

    Added the a.txt file

--- git.js
+++ git.js
@@ -1,5 +1,6 @@
 const git = require("isomorphic-git")
 const fs = require("fs")
+const http = require('isomorphic-git/http/node')
 
 git.add({ fs, dir: './', filepath: '.' }).then(()=>console.log('git add done'));
 
@@ -11,6 +12,17 @@ let sha = git.commit({
     email: 'mrtest@example.com',
   },
   message: 'Added the a.txt file'
-})
-console.log(sha)
+}).then(() => {
+console.log(sha);
+});
+
+let pushResult = git.push({
+  fs,
+  http,
+  dir: './',
+  remote: 'origin',
+  ref: 'main',
+  onAuth: () => ({ username: process.env.GITHUB_TOKEN }),
+}).then((result) => console.log(result))
+.catch((err) => console.log("error", err));
 
