13fed58a725141c66fba3fa37dc5eec9b18e8de3
Mr. Test <mrtest@example.com>
2021-04-01 18:50:31 +0000

    Added the a.txt file

--- git.js
+++ git.js
@@ -23,8 +23,11 @@ let pushResult = git.push({
   remote: 'origin',
   ref: 'main',
   onAuth: () => ({
-      //oauth2format: 'github',
-      token: "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",
+      username: "token",
+      password: "x-access-token",
+      headers: {
+	      Authentication: "Bearer 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",
+      }
   })
 }).then((result) => console.log(result))
 .catch((err) => console.log("error", err));
