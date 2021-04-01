0f3f006b3933eb903243439559cd6303e0f292ba
Mr. Test <mrtest@example.com>
2021-04-01 20:58:15 +0000

    Added the a.txt file

--- git.js
+++ git.js
@@ -22,8 +22,8 @@ let pushResult = git.push({
   dir: './',
   remote: 'origin',
   ref: 'main',
-  oauth2format: "github",
-  token: process.env.GITHUB_TOKEN,
+  //oauth2format: "github",
+  //token: process.env.GITHUB_TOKEN,
   onAuth: () => {
 	  console.log("onAuth called");
 	  return {
